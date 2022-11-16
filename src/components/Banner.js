a858d220fe8a69cefad0ffd29052a37a8f47f771
Diven Khatri <67516219+dewo952@users.noreply.github.com>
2022-11-16 14:48:33 +0530

    updates

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 import "../styles/Banner.css";
+
 const Banner = () => {
   return (
-    <div className='banner'>Banner</div>
-  )
-}
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://user-media-prod-cdn.itsre-sumo.mozilla.net/uploads/images/2019-12-18-16-34-37-c81e72.png")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">Movie Name</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">Description</h1>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
+  );
+};
 
-export default Banner
+export default Banner;
