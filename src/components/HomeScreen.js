7d3335484ff7d77fa610636bf60854ad831ae2a0
Diven Khatri <67516219+dewo952@users.noreply.github.com>
2022-11-18 19:18:46 +0530

    updates

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,15 +1,58 @@
 import React from "react";
 import "../styles/HomeScreen.css";
+import requests from "./api/requests";
 import Banner from "./Banner";
 import NavBar from "./NavBar";
+import Row from "./Row";
 
 const HomeScreen = () => {
-  return <div className="HomeScreen">
-    <NavBar/>
-    <Banner/>
-    
+  return (
+    <div className="homeScreen">
+      <NavBar />
+      <Banner />
 
-  </div>;
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row
+        title="Trending Now"
+        fetchUrl={requests.fetchTrending}
+        isLargeRow
+      />
+      <Row
+        title="Top Rated"
+        fetchUrl={requests.fetchTopRated}
+        isLargeRow
+      />
+      <Row
+        title="Action Movies"
+        fetchUrl={requests.fetchActionMovies}
+        isLargeRow
+      />
+      <Row
+        title="Comedy Movies"
+        fetchUrl={requests.fetchComedyMovies}
+        isLargeRow
+      />
+      <Row
+        title="Horror Movies"
+        fetchUrl={requests.fetchHorrorMovies}
+        isLargeRow
+      />
+      <Row
+        title="Romance Movies"
+        fetchUrl={requests.fetchRomanceMovies}
+        isLargeRow
+      />
+      <Row
+        title="Documentaries"
+        fetchUrl={requests.fetchDocumentaries}
+        isLargeRow
+      />
+    </div>
+  );
 };
 
 export default HomeScreen;
